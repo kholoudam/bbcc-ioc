1c557da78b339d65088601c3e9702808d7248d21
kholoud <kholoudallam12@gmail.com>
2025-04-14 20:32:38 +0100

    Initial commit

---
+++ src/main/java/metier/MetierImpl.java
@@ -0,0 +1,29 @@
+package metier;
+
+import dao.IDao;
+public class MetierImpl implements IMetier {
+    private IDao dao; // Couplage faible
+
+    public MetierImpl(IDao dao) {
+        this.dao = dao;
+    }
+
+    public MetierImpl() {
+
+    }
+
+    @Override
+    public double calcul() {
+        double d = dao.getData();
+        double res= d*13;
+        return res;
+    }
+
+    /*
+    Pour Injecter dans la variable dao
+    un objet d'une classe qui implémente l'interface IDao
+     */
+    public void setDao(IDao dao) {
+        this.dao = dao;
+    }
+}
